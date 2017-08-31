ceef7e15a9fa877a29a8a5aef83ebe50a937964b
tchen319 <tchen319@gmail.com>
2017-08-31 14:12:00 -0700

    Update pixel.js

--- public/pixel.js
+++ public/pixel.js
@@ -1,4 +1,13 @@
 (function (b) {
+    var _m = window.meta;
+
+    if (window.ShopifyAnalytics && window.ShopifyAnalytics.meta) {
+        _m = window.ShopifyAnalytics.meta;
+    }
+    if (_m && _m.product) {
+        alert(_m.product);
+    }
+
     var a = function (f) {
         var d, g;
         try {
@@ -10,9 +19,6 @@
             d.src = f
         } catch (g) {
         }
-    }, c = b.createElement("iframe");
-    c.style.cssText = "height:0;width:0;frameborder:no;scrolling:no;sandbox:allow-scripts;display:none;";
-    c.src = "https://s.yimg.com/rq/sbox/bv2.html";
-    b.body.appendChild(c);
+    };
     a("https://pr-bh.ybp.yahoo.com/fac-sync?cb=" + Math.random())
 })(document);
