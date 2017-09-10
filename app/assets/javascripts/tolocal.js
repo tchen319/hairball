c7a0e81398a2c4eeccd10e42bde56b49b574f869
Tong Chen <tong@oath.com>
2017-09-10 13:36:24 -0700

    add a function def.

--- app/assets/javascripts/tolocal.js
+++ app/assets/javascripts/tolocal.js
@@ -1,6 +1,8 @@
-var query = document.location.search;
-if (query) {
-    onload = setTimeout(function () {
-        document.location = 'http://localhost:4080/oauth/approval' + query;
-    }, 5000);
+function doPostLoad() {
+    var query = document.location.search;
+    if (query) {
+        onload = setTimeout(function () {
+            document.location = 'http://localhost:4080/oauth/approval' + query;
+        }, 5000);
+    }
 }
