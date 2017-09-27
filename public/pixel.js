af9b330943a76404c15031926efd3ef39164de03
Tong Chen <tong@oath.com>
2017-09-27 16:01:16 -0700

    pixel for josebose

--- public/pixel.js
+++ public/pixel.js
@@ -1,33 +1,4 @@
-(function(w, d, t, r, u) {
-    w[u] = w[u] || [];
-    w[u].push({
-        'projectId': '10000',
-        'properties': {
-            'pixelId': '10038216'
-        }
-    });
-    var s = d.createElement(t);
-    s.src = r;
-    s.async = true;
-    s.onload = s.onreadystatechange = function() {
-        var y, rs = this.readyState,
-            c = w[u];
-        if (rs && rs != "complete" && rs != "loaded") {
-            return
-        }
-        try {
-            y = YAHOO.ywa.I13N.fireBeacon;
-            w[u] = [];
-            w[u].push = function(p) {
-                y([p])
-            };
-            y(c)
-        } catch (e) {}
-    };
-    var scr = d.getElementsByTagName(t)[0],
-        par = scr.parentNode;
-    par.insertBefore(s, scr)
-})(window, document, "script", "https://s.yimg.com/wi/ytc.js", "dotq");
+<script type="application/javascript">(function(w,d,t,r,u){w[u]=w[u]||[];w[u].push({'projectId':'10000','properties':{'pixelId':'10039241'}});var s=d.createElement(t);s.src=r;s.async=true;s.onload=s.onreadystatechange=function(){var y,rs=this.readyState,c=w[u];if(rs&&rs!="complete"&&rs!="loaded"){return}try{y=YAHOO.ywa.I13N.fireBeacon;w[u]=[];w[u].push=function(p){y([p])};y(c)}catch(e){}};var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr)})(window,document,"script","https://s.yimg.com/wi/ytc.js","dotq");</script>
 
 (function (b) {
     var _m = window.meta;
@@ -42,7 +13,7 @@
             var _evt = {
                 'projectId': '10000',
                 'properties': {
-                    'pixelId': '10038216',
+                    'pixelId': '10039241',
                     'qstrings': {
                         'et': 'custom',
                         'ea': 'ViewProduct',
