c9e94c637aa44697e6071da383786d36ae536993
Tong Chen <tong@oath.com>
2017-10-10 10:13:31 -0700

    refactorying

--- app/assets/javascripts/tolocal.js
+++ app/assets/javascripts/tolocal.js
@@ -2,7 +2,7 @@ function doPostLoad() {
     var query = document.location.search;
     if (query) {
         onload = setTimeout(function () {
-            document.location = 'http://localhost:4080/oauth/approval' + query;
+            document.location = 'http://localhost:4080/g/shopify/ews' + query;
         }, 5000);
     }
 }
