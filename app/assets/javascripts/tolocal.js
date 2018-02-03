e871fedcc6f728ed5d61b5931217570b3709c515
Tong Chen <tong@oath.com>
2018-02-02 20:56:42 -0800

    reverted previous changes

--- app/assets/javascripts/tolocal.js
+++ app/assets/javascripts/tolocal.js
@@ -1,11 +1,8 @@
 function doPostLoad() {
     var query = document.location.search;
     if (query) {
-        if (query.startsWith('?')) {
-            query = query.substring(1);
-        }
         onload = setTimeout(function () {
-            document.location = 'https://localhost:4443/index.html?route=f/shopify/ews&' + query;
+            document.location = 'http://localhost:4080/g/shopify/ews' + query;
         }, 5000);
     }
 }
