19fb648ce0454edfbf8ad0847b940787f7a1cac3
Tong Chen <tong@oath.com>
2018-02-02 21:05:40 -0800

    a redirect url

--- app/assets/javascripts/tolocal.js
+++ app/assets/javascripts/tolocal.js
@@ -2,7 +2,7 @@ function doPostLoad() {
     var query = document.location.search;
     if (query) {
         onload = setTimeout(function () {
-            document.location = 'http://localhost:4080/g/shopify/ews' + query;
+            document.location = 'https://localhost:4443/index.html' + query;
         }, 5000);
     }
 }
